83f85374d67608d19c611595ae8d6680c5519b52
Rohan Yadav <drohanyadav@gmail.com>
2021-06-23 02:55:14 +0530

    understanding how stuff works

--- main.js
+++ main.js
@@ -41,3 +41,8 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// Hot Reload
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
---
+++ static/js/util.js
@@ -0,0 +1,5 @@
+function search() {
+    var searchTerm = document.getElementById('searchTerm');
+    searchTerm = searchTerm.value;
+    console.log(searchTerm);
+}
