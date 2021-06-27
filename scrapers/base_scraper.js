dee178242245a50cb08d3832c53b7572287f59dc
Kartikey Singh <kartikeysinghc@gmail.com>
2021-06-27 18:27:01 +0530

    calling functions using eval

--- scrapers/base_scraper.js
+++ scrapers/base_scraper.js
@@ -1,29 +1,20 @@
 const scraper_fitgirl = require("./scraper_fitgirl.js");
 const scraper_steamunlocked = require("./scraper_steamunlocked.js");
 
-async function scrapeSites(category) {
-    // define all available functions for each category here
-    var scraperCategories = {
-        games: {
-            fitgirl: scraper_fitgirl,
-            steamunlocked: scraper_steamunlocked,
-        },
-        anime: {},
-        movies: {},
-    };
-
-    // filter functions for specific category
-    var scraperFunctions = scraperCategories[category];
-
+async function scrapeSites() {
     var selectedWebsite = document.querySelector(".selectWebsite").value;
     var searchSection = document.querySelector(".searchTerm").parentNode;
 
     if (selectedWebsite == "all") {
-        for (var eachFunc in scraperFunctions) {
-            await scraperFunctions[eachFunc](searchSection);
+        var scraperFunctions = document.querySelectorAll(
+            ".selectWebsite option:not(:first-child)"
+        );
+        for (var eachFunc of scraperFunctions) {
+            var funcName = eachFunc.value;
+            await eval(funcName)(searchSection);
         }
     } else {
-        await scrapeFunctions[siteSelection](searchSection);
+        await eval(selectedWebsite)(searchSection);
     }
 }
 
